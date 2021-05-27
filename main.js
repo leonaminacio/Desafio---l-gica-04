aa6721948e7003fab64c0f0e63ca3dfca8d5f99a
leonaminacio <leonamalves46@gmail.com>
2021-05-27 02:28:31 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function bom() {
+    console.log("Bom dia")
+}
+
+bom()
+
+function dados(nome){
+    console.log("Meu nome é", nome)
+}
+
+dados("Leonam")
+
+function favoritos(filme, musica){
+    console.log(`Meu filme favorito é ${filme} e Minha musica favorita é ${musica}`)
+}
+
+favoritos("Velozes e furiosos", " muleque de vila")
+
+function m1(){
+
+    console.log(5*3)
+}
+
+m1(5)
+
